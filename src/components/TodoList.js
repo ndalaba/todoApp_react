f3554458b1dd9b74aa566ff95c2f4aa0184aff3b
N'Dalaba Diallo <dmn@dev-hoster.com>
2018-03-31 18:27:28 +0200

    todo app

---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+const Todo = ({ todo, remove }) => {
+
+    return (<li onClick={()=> remove(todo.id)}> { todo.text }</li >);
+}
+
+const TodoList = ({ todos, remove }) => {
+    const todoNode = todos.map((todo) => {
+        return (<Todo todo={todo} key={todo.id} remove={remove} />)
+    });
+    return (<ul>{todoNode}</ul>);
+}
+
+export default TodoList;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+//import TodoApp from './components/TodoApp';
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <h1>coucou tout lem onde </h1>,
+    document.getElementById('root')
+);
